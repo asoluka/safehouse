9b30fe097d36cdcd0a86f28d517464ec27cc19de
TC Asoluka <stracool9@gmail.com>
2020-04-01 19:24:21 +0100

    SafeHouse
    For the SafeHouse Project. This website is not responsive yet. View with a Laptop computer.

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/bootstrap-4.4.1/css/bootstrap.css">
+    <link rel="stylesheet" href="styles/custom/style.css">
+</head>
+<body>
+    <main>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light ml-sm-5 mr-sm-5">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <a class="navbar-brand ml-sm-5" href="#">
+                    <img src="assets/logo@2x.png" alt="logo">
+                </a>
+                <ul class="navbar-nav mt-2 mt-lg-0 ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">How It Works</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SafePay</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FAQs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn green nav-link" href="#">Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <section class="purchase-property">
+            <div class="container-fluid">
+                <div class="row full-height">
+                    <!-- <div class="col-sm-1"></div> -->
+                    <div class="col-5 f-center-vr height-inherit">
+                        <div class="row">
+                            <div class="col-sm-1"></div>
+                            <div class="col-10">
+                                <div class="row">
+                                    <div class="col-sm-1"></div>
+                                    <div class="col-sm-10">
+                                        <h2>Purchase a property in Nigeria from anywhere in the world</h2>
+                                        <p>We connect you - the buyer to verified real estate companies in Nigeria</p>
+                                    </div>
+                                    <div class="col-sm-1"></div>
+                                </div>
+                            </div>
+                            <div class="col-sm-1"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 mt-5 shadow input-container">
+                                <div class="input-group input-group-lg banner-input-group">
+                                    <input type="text" class="banner-input form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+                                    <div class="input-group-append">
+                                        <span class="input-group-text green" id="inputGroup-sizing-lg">Search Listing</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-7 height-inherit">
+                        <div class="row hero-section">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="how-it-works">
+            <div class="container pt-5 pb-5">
+                <!-- <div class=""> -->
+                    <section class="row hiw-heading">
+                        <div class="col-12 col-sm-4"></div>
+                        <div class="col-12 col-sm-4">
+                            <h2>How it works</h2>
+                            <p>We connect you - the buyer to verified real estate companies in Nigeria</p>
+                        </div>
+                        <div class="col-12 col-sm-4"></div>
+                    </section>
+
+                    <section class="row hiw-groups pt-5 pb-5">
+                        <div class="col-12 col-sm-4">
+                            <div class="step-card-square col-12 mb-4 pt-5 pb-3 pl-5 pr-5">
+                                <span class="step-number one">1</span>
+                                <h4>Sign up and fill KYC form</h4>
+                                <p>Register and fill a detailed KYC form to enable us confirm your identity.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-4">
+                            <div class="step-card-square col-12 mb-4 pt-5 pb-3  pl-5 pr-5">
+                                <span class="step-number two">2</span>
+                                <h4>Prospect from our listings</h4>
+                                <p>Interact with our listings depending on your interest.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-4">
+                            <div class="step-card-square col-12 mb-4 pt-5 pb-3  pl-5 pr-5">
+                                <span class="step-number three">3</span>
+                                <h4>Agree to Terms</h4>
+                                <p>Subscriber and Property Company agree to secure a transaction via safeHouse Escrow (SafePay).</p>
+                            </div>                        
+                        </div>
+                        <div class="col-12 col-sm-4">
+                            <div class="step-card-square col-12 mb-4 pt-5 pb-3  pl-5 pr-5">
+                                <span class="step-number four">4</span>
+                                <h4>Payment to Escrow</h4>
+                                <p>Subscriber makes payment into SafePay, money is held in escrow until all obligations are fulfilled.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-4">
+                            <div class="step-card-square col-12 mb-4 pt-5 pb-3  pl-5 pr-5">
+                                <span class="step-number five">5</span>
+                                <h4>Receive Property Documents</h4>
+                                <p>Subscriber receives property documents via dedicated courier on due date as agreed.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-4">
+                            <div class="step-card-square col-12 mb-4 pt-5 pb-3  pl-5 pr-5">
+                                <span class="step-number six">6</span>
+                                <h4>Approval as show satisfaction</h4>
+                                <p>Subscriber confirms that all obligations have been fulfilled as expected by approving the transaction in their dashboard</p>
+                            </div>
+                        </div>
+                        <!-- mb-4 pt-5 pb-3 pl-5 pr-5 -->
+                        <div class="col-12"> 
+                            <div class="col-12">
+                            <div class="row step-card-rectangle mb-4 pl-5 pr-5 pt-5 pb-5">
+                                <div class="col-12 col-sm-1 f-center-vr">
+                                    <div class="row"><span class="step-number seven">7</span></div>
+                                </div>
+                                <div class="col-12 col-sm-8 f-center-vr">
+                                    <div class="row">
+                                        <h4 class="heading-exempt">Escrow release to Property Company</h4>
+                                        <p>Funds are debited from the escrow and made availabe for withdrawal in the property company's account.</p>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-3 f-center">
+                                    <!-- <div class="row"> -->
+                                        <button class="btn btn-lg btn-block green">Get Started</button>
+                                    <!-- </div> -->
+                                </div>
+                            </div>
+                            </div>
+                            
+                        </div>
+                    </section>
+                <!-- </div> -->
+            </div>
+        </section>
+
+        <section class="who-we-are">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-sm-7 wwa-first-part">
+                        <div class="row">
+                            <img class="img-fluid" src="img/who-we-are.png" alt="Houses">
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-5 wwa-second-part f-center-vr pr-4 pb-5">
+                        <h2>Who We Are</h2>
+                        <p>SafeHouse is focused on providing relief to the trust challenge being experienced in the Real 
+                            Estate ecosystem in Nigeria through the provision of necessary knowledge, information, 
+                            interaction and finally payment services needed to enhance the process of purchasing 
+                            properties.
+                        </p>
+
+                        <p>We connect you - the buyer to verified real estate companies in Nigeria. Through safehouse, 
+                            millions of Nigerians and investors can purchase a property in Nigeria anywhere in the world 
+                            without fear.
+                        </p>
+                        <button class="btn green read-more">Read More</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="pb-5 pt-3">
+            <div class="container three-step-process">
+                <div class="row mb-5">
+                    <div class="col-12 col-sm-4"></div>
+                    <div class="col-12 col-sm-4">
+                        <h2>The Three-Step Process</h2>
+                    </div>
+                    <div class="col-12 col-sm-4"></div>
+                </div>
+
+                <div class="row step-first-grid">
+                    <div class="col-12 col-sm-2"></div>
+                    <div class="col-12 col-sm-4 f-center-vr pl-5">
+                        <span>
+                            <img class="img-fluid step-rectangle-1" src="assets/secure.png" alt="Step 1 Secure">
+                        </span>
+                        <h4>Sign up and fill KYC form</h4>
+                        <p>Register and fill a detailed KYC form to enable us confirm your identity.</p>
+                    </div>
+                    <div class="col-12 col-sm-4">
+                        <div class="row">
+                            <img class="img-fluid" src="img/step 1@2x.png" alt="Step 1 Image">
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-2"></div>
+                </div>
+
+                <div class="row step-second-grid">
+                    <div class="col-12 col-sm-2"></div>
+                    <div class="col-12 col-sm-4">
+                        <div class="row">
+                            <img class="img-fluid" src="img/step 2@2x.png" alt="Step 1 Image">
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-4 f-center-vr pl-5">
+                        <span>
+                            <img class="img-fluid step-rectangle-2" src="assets/secure.png" alt="Step 1 Secure">
+                        </span>
+                        <h4>Prospect from our listings</h4>
+                        <p>Register and fill a detailed KYC form to enable us confirm your identity.</p>
+                    </div>
+                    <div class="col-12 col-sm-2"></div>
+                </div>
+
+                <div class="row step-third-grid">
+                    <div class="col-12 col-sm-2"></div>
+                    <div class="col-12 col-sm-4 f-center-vr pl-5">
+                        <span>
+                            <img class="img-fluid step-rectangle-3" src="assets/secure.png" alt="Step 1 Secure">
+                        </span>
+                        <h4>Receive Property Documents</h4>
+                        <p>Register and fill a detailed KYC form to enable us confirm your identity</p>
+                    </div>
+                    <div class="col-12 col-sm-4">
+                        <div class="row">
+                            <img class="img-fluid" src="img/step 3@2x.png" alt="Step 1 Image">
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-2"></div>
+                </div>
+            </div>
+        </section>
+
+        <section class="need-more-information">
+            <div class="container mt-5 mb-5">
+                <div class="row text-center green more-info-card">
+                    <div class="col-12 pt-3 pb-5">
+                        <h2>Need more information</h2>
+                        <p>Let us know what you need to have a successful <br> transaction with ease and peace of mind.</p>
+                        <button class="btn btn-light">Read More</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="pt-5 pb-5">
+            <div class="container pb-5">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="row">
+                            <div class="col-12">
+                                <img class="img-fluid" src="assets/logo@2x.png">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="mt-5 mb-5">
+                                    SafeHouse is focused on providing relief to the trust challenge being experienced in the Real 
+                                Estate ecosystem in Nigeria through the provision of necessary knowledge, information, 
+                                interaction and finally payment services needed to enhance the process of purchasing 
+                                properties.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <ul class="footer-links">
+                            <li>Customers</li>
+                            <li>Terms & Conditions</li>
+                            <li>SafeHouse Guarantee</li>
+                            <li>Cancellation Policy</li>
+                            <li>Legal</li>
+                            <li>SafeHouse Opportunities</li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <ul class="footer-links">
+                            <li>Company </li>
+                            <li>About</li>
+                            <li>FAQs</li>
+                            <li>How it Works</li>
+                            <li>Our Benefits</li>
+                            <li>Testimonals</li>
+                        </ul>
+                    </div>
+
+                    <div class="col-3 newsletter">
+                        <h4>NEWSLETTER</h4>
+                        <p>Enter your email address to receive the latest information about SafeHouse</p>
+                        <div class="col-12 mt-5 shadow">
+                            <div class="input-group input-group-lg banner-input-group">
+                                <input type="text" class="banner-input form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+                                <div class="input-group-append">
+                                    <span class="input-group-text green" id="inputGroup-sizing-lg">
+                                        <img src="assets/send.png" alt="">
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <h4>Social Media</h4>
+                        <div class="row pl-3">
+                            <div class="col-3 f-center shadow social-media">
+                                <img src="assets/instagram.png" alt="social media">
+                            </div>
+                            <div class="col-3 f-center shadow social-media">
+                                <img src="assets/facebook.png" alt="social media">
+                            </div>
+                            <div class="col-3 f-center shadow social-media">
+                                <img src="assets/twitter.png" alt="social media">
+                            </div>
+                            <div class="col-3 f-center shadow social-media">
+                                <img src="assets/whatsapp.png" alt="social media">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
